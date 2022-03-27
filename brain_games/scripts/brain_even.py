dff69d52a4239134081a453a22b9760a63db838e
yauhen1996 <yauhen1996@WIN-6RO4OS41KBA.localdomain>
2022-03-27 16:47:36 +0300

    games

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -12,21 +12,22 @@ def random_num():
     print('Hello, {}!'.format(name))
     print('Answer "yes" if the number is even, otherwise answer "no".')
     i = 0
+    correct = "'yes' is wrong answer ;(. Correct answer was 'no'."
     while i <= 2:
         a = randint(1, 100)
-        print('Question:', a )
+        print('Question:', a)
         b = input('Your answer: ')
         if a % 2 == 0 and b == 'yes' or a % 2 != 0 and b == 'no':
             print('Correct!')
             i += 1
         if a % 2 == 0 and b != 'yes' or a % 2 != 0 and b != 'no':
-            return print("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {}!".format(name))
+            return print(correct + "\nLet's try again, {}!".format(name))
     return print('Congratulations, {}!'.format(name))
-    
+
 
 def main():
     random_num()
-    
+
 
 if __name__ == '__main__':
     main()
