e9e8d6de2ae5f35e37c7abf4510589ed261af35d
yauhen1996 <getthe798@gmail.com>
2022-04-14 17:38:03 +0300

    even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -21,13 +21,11 @@ def random_num():
         answer = input('Your answer: ')
         answer_yes = 'yes'
         answer_no = 'no'
-        if num % 2 == 0 and answer == answer_yes:
+        if (num % 2 == 0 and answer == answer_yes) or (num % 2 != 0 and answer == answer_no):
             print('Correct!')
-        elif num % 2 != 0 and answer == answer_no:
-            print('Correct')
         elif num % 2 == 0 and answer != answer_yes:
-            return print(f"'{answer}' is wrong answer ;(. Correct answer was '{answer_yes}'." + "\nLet's try again, {}!".format(name))
+            return print(f"'{answer}' is wrong answer ;(. Correct answer was '{answer_yes}'.\nLet's try again, {name}!")
         elif num % 2 != 0 and answer != answer_no:
-            return print(f"'{answer}' is wrong answer ;(. Correct answer was '{answer_no}'." + "\nLet's try again, {}!".format(name))
+            return print(f"'{answer}' is wrong answer ;(. Correct answer was '{answer_no}'.\nLet's try again, {name}!")
         i += 1
-    return print('Congratulations, {}!'.format(name))
+    return print(f'Congratulations, {name}!')
